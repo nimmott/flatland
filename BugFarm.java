ccf12d2028c593d3bcffa80f9dca19004ac44cca
Harrison Brace <hbrace@gmail.com>
2016-08-25 21:04:45 -0500

    initial commit

---
+++ BugFarm.java
@@ -0,0 +1,146 @@
+package flatland;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hbrace
+ */
+public class BugFarm {
+
+    private int initNumBugs;
+    private int initNumMales;
+    private int initNumFemales;
+    private int range;
+
+    private int aliveBugs;
+    private int deadBugs;
+    private int maleBugs;
+    private int femaleBugs;
+    private int aliveMale;
+    private int aliveFemale;
+    private int deadMale;
+    private int deadFemale;
+
+    private ArrayList<Bug> bugList;
+
+    public BugFarm() {
+
+    }
+
+    public BugFarm(int numBugs, int range) {
+        initNumBugs = numBugs;
+        this.range = range;
+
+        bugList = new ArrayList<Bug>();
+
+        for (int i = 0; i < numBugs; i++) {
+            bugList.add(new Bug(range));
+        }
+        initNumMales = getMales();
+        initNumFemales = getFemales();
+    }
+
+    public void getGenderCount() {
+        maleBugs = 0;
+        femaleBugs = 0;
+        for (int i = 0; i < bugList.size(); i++) {
+            boolean temp = bugList.get(i).getMale();
+            if (temp) {
+                maleBugs++;
+            } else {
+                femaleBugs++;
+            }
+        }
+
+    }
+
+    public int getMales() {
+        getGenderCount();
+        return maleBugs;
+    }
+
+    public int getFemales() {
+        getGenderCount();
+        return femaleBugs;
+    }
+
+    public void updateStats() {
+        aliveBugs = 0;
+        deadBugs = 0;
+        maleBugs = 0;
+        femaleBugs = 0;
+        aliveFemale = 0;
+        aliveMale = 0;
+        deadFemale = 0;
+        deadMale = 0;
+
+        for (int i = 0; i < bugList.size(); i++) {
+            boolean male = bugList.get(i).getMale();
+            boolean alive = bugList.get(i).getIsAlive();
+            if (alive) {
+                aliveBugs++;
+            }
+            if (!alive) {
+                deadBugs++;
+            }
+            if (male) {
+                maleBugs++;
+            }
+            if (!male) {
+                femaleBugs++;
+            }
+            if (male && alive) {
+                aliveMale++;
+            }
+            if (male && !alive) {
+                deadMale++;
+            }
+            if (!male && alive) {
+                aliveFemale++;
+            }
+            if (!male && !alive) {
+                deadFemale++;
+            }
+
+        }
+
+    }
+
+    public void moveBugs() {
+        for (int i = 0; i < bugList.size(); i++) {
+            bugList.get(i).move();
+        }
+    }
+
+    public void fightOrMate() {
+        int limit = bugList.size();
+
+        for (int k = 0; k < limit; k++) {
+            for (int o = k + 1; o < limit; o++) {
+                if (bugList.get(k).equals(bugList.get(o))
+                        && bugList.get(k).getIsAlive() && bugList.get(o).getIsAlive()) {
+                    if (bugList.get(k).getMale() == bugList.get(o).getMale()) {
+                        //fight
+                        //System.out.println("One bug must die");
+                        double temp = Math.random();
+                        if (temp >= .5) {
+                            bugList.get(k).killBug();
+                            //  System.out.println("Bug killed");
+                        }
+                        if (temp < .5) {
+                            bugList.get(o).killBug();
+                            //   System.out.println("Bug killed");
+                        }
+                    }
+                }
+                if (bugList.get(k).getMale() != bugList.get(o).getMale()
+                        && bugList.get(k).equals(bugList.get(o))) {
+                    Bug temp = new Bug(range);
+                    bugList.add(temp);
+                    // System.out.println("new bug added");
+                }
+            }
+        }
+    }
+}
---
+++ Flatland.java
@@ -0,0 +1,67 @@
+// Your Name: Harrison Brace   
+// Your CLID: HAB5597
+// CMPS 360
+// Programming Project : #
+// Due Date :
+// Program Description: (insert description here)
+// Certificate of Authenticity:
+// I certify that the code in the method functions including
+// method function main of this project are entirely my own
+// work.
+
+package flatland;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hbrace
+ */
+public class Flatland {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int numBugs;
+        String filename;
+        int range;
+        int numMoves;
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println ("What is the inital number of bugs for the bugfarm?");
+        numBugs = scan.nextInt();
+        System.out.println ("What is the range of the bugs int the farm?");
+        System.out.println("Please enter a number from 0 to 99:");
+        range = scan.nextInt();
+        System.out.println ("How many times should the bugs move?");
+        numMoves = scan.nextInt();
+        if (range < 0 || range > 99){
+            System.out.println("Number was outside range");
+            return;
+        }
+        System.out.println("What is the name of the report file?");
+        filename = scan.next();
+        BugFarm farm = new BugFarm (numBugs, range);
+ //       farm.updateStats();
+ 
+        for (int i=0;i<numMoves;i++){
+            farm.moveBugs();
+            farm.fightOrMate();
+        }
+       
+
+
+
+         farm.moveBugs();
+        farm.fightOrMate();
+        farm.moveBugs();
+        farm.fightOrMate();
+        farm.updateStats();
+        
+        System.out.println("Hello World");
+        // TODO code application logic here
+    }
+
+}
